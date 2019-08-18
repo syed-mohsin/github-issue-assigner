7d3769b376b3e57764428541c629cfe9033129e2
Syed Mohsin <syedm.90@gmail.com>
2019-08-18 13:33:18 -0700

    add body parser and fix receiving webhook data

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 6001
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.json({
 	test: 1,
 }))
+
+app.post('/', (req, res) => {
+	const action = req.body.action
+	const { id, number } = req.body.issue
+
+	const payload = {
+		success: true,
+		action: action,
+		issueId: id,
+		issueNumber: number,
+	}
+
+	res.json(payload)
+})
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}!`)
