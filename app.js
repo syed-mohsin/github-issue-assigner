0658c3a56f53e9c64e617225603244f0594db993
Syed Mohsin <syedm.90@gmail.com>
2019-08-18 19:02:45 -0700

    add redis state to implement round robin assigning algo

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const axios = require('axios')
+const redis = require('redis')
 
 const port = process.env.PORT || 6001
-
 const ISSUES_API_ENDPOINT = 'https://api.github.com/repos/syed-mohsin/github-issue-assigner/issues'
 axios.defaults.headers.post['Authorization'] = `token ${process.env.GITHUB_API_ACCESS_TOKEN}`
+
+const redisClient = redis.createClient(process.env.REDIS_URL)
+redisClient.on("error", err => {
+    console.log("Redis Error:", err)
+});
 
 const USERNAMES = ['syed-mohsin']
 
 app.use(bodyParser.json())
-
-app.get('/', (req, res) => res.json({
-	test: 1,
-}))
 
 app.post('/', (req, res) => {
 	const action = req.body.action
@@ -28,14 +29,29 @@ app.post('/', (req, res) => {
 	}
 
 	if (action === 'opened') {
-		axios.post(`${ISSUES_API_ENDPOINT}/${number}/assignees`, {
-			assignees: USERNAMES
-		})
-		.then(res => {
-			console.log('res', res)
-		})
-		.catch(err => {
-			console.log('err', err)
+		redisClient.watch('assigneeIndex', err => {
+			if (err) {
+				return console.warn('err', err)
+			}
+
+			redisClient.get('assigneeIndex', (err, res) => {
+				if (err) {
+					return console.warn('err', err)
+				}
+
+				const assigneeIndex = Number(res) || 0
+				axios.post(`${ISSUES_API_ENDPOINT}/${number}/assignees`, {
+					assignees: USERNAMES[assigneeIndex]
+				}).then(() => {
+					redisClient.multi()
+						.set('assigneeIndex', (assigneeIndex + 1) % USERNAMES.length)
+						.exec((err) => {
+							if (err) {
+								return console.warn('err', err)
+							}
+						})
+				})
+			})
 		})
 	}
 
