38e3a141d8c5b750ae6066850c78323efbfac171
Syed Mohsin <syedm.90@gmail.com>
2019-08-18 17:47:45 -0700

    add assignee post request

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const request = require('request')
 const port = process.env.PORT || 6001
+
+const USERNAMES = ['syed-mohsin']
+const ISSUES_API_ENDPOINT = 'https://api.github.com.com/repos/syed-mohsin/github-issue-assigner/issues'
 
 app.use(bodyParser.json())
 
@@ -20,9 +24,24 @@ app.post('/', (req, res) => {
 		issueNumber: number,
 	}
 
+	if (action === 'opened') {
+		request({
+			url: `${ISSUES_API_ENDPOINT}/${number}/assignees`,
+			method: 'POST',
+			headers: {
+				Authorization: `token ${process.env.GITHUB_API_ACCESS_TOKEN}`,
+			},
+			json: { assignees: USERNAMES },
+			// followAllRedirects: true,
+		}, (err, response, body) => {
+			console.log('error', err, 'status', response && response.statusCode, 'body', body)
+		})
+	}
+
 	res.json(payload)
 })
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}!`)
+	console.log('env', process.env.GITHUB_API_ACCESS_TOKEN)
 })
